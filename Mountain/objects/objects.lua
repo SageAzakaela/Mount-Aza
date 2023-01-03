59401c0d6d4882002ca1a23b52b6fae5ab4a9475
SageAzakaela <sageazakaela@gmail.com>
2023-01-03 13:57:34 -0700

    Initial Commit
    If you have problems getting the buildings to show, make sure to check the PZW file; the path may have changed -- simply redirect the path and it should work.

---
+++ Mountain/objects/objects.lua
@@ -0,0 +1,12 @@
+objects = {
+  { name = "", type = "DeepForest", x = 1, y = 0, z = 0, width = 299, height = 299 },
+  { name = "", type = "DeepForest", x = 32, y = 15, z = 1, width = 248, height = 231 },
+  { name = "", type = "DeepForest", x = 40, y = 31, z = 2, width = 226, height = 191 },
+  { name = "", type = "DeepForest", x = 67, y = 46, z = 3, width = 188, height = 161 },
+  { name = "", type = "DeepForest", x = 76, y = 58, z = 4, width = 181, height = 141 },
+  { name = "", type = "DeepForest", x = 87, y = 74, z = 5, width = 112, height = 95 },
+  { name = "", type = "DeepForest", x = 107, y = 92, z = 6, width = 46, height = 43 },
+  { name = "", type = "DeepForest", x = 112, y = 93, z = 7, width = 31, height = 27 },
+  { name = "", type = "DeepForest", x = 173, y = 266, z = 7, width = 1, height = 1 },
+  { name = "", type = "SpawnPoint", x = 204, y = 147, z = 3, width = 1, height = 1, properties = { Professions = "all" } }
+}
---
+++ Mountain/objects/spawnpoints.lua
@@ -0,0 +1,22 @@
+function SpawnPoints()
+return {
+  constructionworker = {
+    { worldX = 0, worldY = 0, posX = 204, posY = 147, posZ = 3 }
+  },
+  fireofficer = {
+    { worldX = 0, worldY = 0, posX = 204, posY = 147, posZ = 3 }
+  },
+  parkranger = {
+    { worldX = 0, worldY = 0, posX = 204, posY = 147, posZ = 3 }
+  },
+  policeofficer = {
+    { worldX = 0, worldY = 0, posX = 204, posY = 147, posZ = 3 }
+  },
+  securityguard = {
+    { worldX = 0, worldY = 0, posX = 204, posY = 147, posZ = 3 }
+  },
+  unemployed = {
+    { worldX = 0, worldY = 0, posX = 204, posY = 147, posZ = 3 }
+  }
+}
+end
